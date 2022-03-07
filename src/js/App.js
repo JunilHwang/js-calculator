e6eca288586a97c22486ff6b61e5a8cad475b37f
junilHwang <junil.h@kakao.com>
2022-03-07 22:07:59 +0900

    feat: 숫자 입력에 대한 이벤트 작성 완료

--- src/js/App.js
+++ src/js/App.js
@@ -1,21 +1,42 @@
 import { Component } from "./core";
 
 export class App extends Component {
+  initState() {
+    return {
+      stack: [0],
+      stackTop: 0,
+    };
+  }
+
+  get lastStackValue() {
+    const { stack, stackTop } = this.state;
+    return stack[stackTop];
+  }
+
+  handleNumberClick = (event) => {
+    const value = Number(event.target.innerHTML.trim());
+    this.setState({
+      stack: [Number(`${this.lastStackValue}${value}`)],
+    });
+  };
+
+  mounted() {
+    this.addEvent("click", ".digit", this.handleNumberClick);
+  }
+
   render() {
     return `
       <div class="calculator">
-        <h1 id="total">0</h1>
+        <h1 id="total">${this.lastStackValue}</h1>
         <div class="digits flex">
-          <button class="digit">9</button>
-          <button class="digit">8</button>
-          <button class="digit">7</button>
-          <button class="digit">6</button>
-          <button class="digit">5</button>
-          <button class="digit">4</button>
-          <button class="digit">3</button>
-          <button class="digit">2</button>
-          <button class="digit">1</button>
-          <button class="digit">0</button>
+          ${Array(10)
+            .fill(9)
+            .map(
+              (v, k) => `
+            <button class="digit">${v - k}</button>
+          `
+            )
+            .join("")}
         </div>
         <div class="modifiers subgrid">
           <button class="modifier">AC</button>
--- src/js/core/Component.js
+++ src/js/core/Component.js
@@ -1,37 +1,59 @@
 export class Component {
   $el;
   props;
-  #state = {};
+  #state;
 
   constructor($el, props = {}) {
     this.$el = $el;
     this.props = props;
+    this.#state = this.initState();
     this.#render();
+  }
+
+  initState() {
+    return {};
   }
 
   get state() {
     return Object.entries(this.#state).reduce((state, [key, value]) => {
       Object.defineProperty(state, key, {
-        get() {
-          return value;
-        },
+        value,
+        writable: false,
       });
       return state;
     }, {});
   }
 
   setState(state) {
-    this.#state = { ...state };
+    this.#state = { ...this.#state, ...state };
     this.#render();
   }
 
   #render() {
     requestAnimationFrame(() => {
-      this.$el.innerHTML = this.render();
+      this.beforeMount();
+      this.$el.innerHTML = this.render().trim();
+      this.mounted();
     });
   }
 
   render() {
-    return "";
+    throw new Error(
+      `${this.constructor.name} class에 render method가 필요합니다.`
+    );
+  }
+
+  beforeMount() {}
+
+  mounted() {}
+
+  addEvent(eventType, selector, eventListener) {
+    this.$el
+      .querySelectorAll(selector)
+      .forEach(($target) => $target.addEventListener(eventType, eventListener));
+  }
+
+  toString() {
+    return this.render();
   }
 }
